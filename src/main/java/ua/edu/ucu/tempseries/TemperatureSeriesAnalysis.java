24622da19ba90a95582fdf25ab9b1f5192a2a287
etamin-code <physicist.o.kuhar@gmail.com>
2020-11-12 20:00:55 +0200

    tuning style

--- src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
+++ src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
@@ -4,57 +4,62 @@ package ua.edu.ucu.tempseries;
 import java.util.InputMismatchException;
 
 public class TemperatureSeriesAnalysis {
+    private double LOWESTTEMP = -273.0;
     private double[] temperatureList;
     private int numOfElements;
     private Object IllegalArgumentException;
 
-    public TemperatureSeriesAnalysis(){
-        temperatureList = new double[4];
+    public TemperatureSeriesAnalysis() {
+        int DEFAULTS = 4;
+        temperatureList = new double[DEFAULTS];
         numOfElements = 0;
     }
 
     public TemperatureSeriesAnalysis(double[] temperatureSeries) {
-        for (double el: temperatureSeries)
-            if (el < -273)
+        for (double el: temperatureSeries) {
+            if (el < LOWESTTEMP) {
                 throw new InputMismatchException();
+            }
+        }
         temperatureList = temperatureSeries;
         numOfElements = temperatureSeries.length;
     }
 
-    public double average(){
-        if (numOfElements == 0)
+    public double average() {
+        if (numOfElements == 0) {
             return 0.0;
+        }
         double sum = 0;
-        for (double el: temperatureList){
+        for (double el: temperatureList) {
             sum += el;
         }
         return sum / numOfElements;
     }
 
-    public double deviation(){
+    public double deviation() {
         if (numOfElements == 0)
             return 0.0;
         double avr = average();
         double sumSquaredDevitaion = 0.0;
-        for (double el: temperatureList){
+        for (double el: temperatureList) {
             sumSquaredDevitaion += (avr - el) * (avr - el);
         }
         return sumSquaredDevitaion / numOfElements;
 
     }
 
-    public double min(){
+    public double min() {
         double minValue = temperatureList[0];
-        for (double el: temperatureList){
+        for (double el: temperatureList) {
             if (el < minValue)
                 minValue = el;
         }
         return minValue;
     }
 
-    public double max(){
-        double maxValue = -273.0;
-        for (double el: temperatureList){
+    public double max() {
+        double maxValue = LOWESTTEMP;
+        for (double el: temperatureList) {
             if (el < maxValue)
                 maxValue = el;
         }
@@ -62,24 +67,24 @@ public class TemperatureSeriesAnalysis {
     }
 
 
-    public double findTempClosestToZero(){
+    public double findTempClosestToZero() {
         return findTempClosestToValue(0.0);
     }
 
-    public double findTempClosestToValue(double tempValue){
+    public double findTempClosestToValue(double tempValue) {
         double valueDif = Math.abs(temperatureList[0] - tempValue);
-        for (double el: temperatureList){
+        for (double el: temperatureList) {
             if (Math.abs(el - tempValue) < valueDif)
                 valueDif = Math.abs(el - tempValue);
         }
         return valueDif;
     }
 
-    public double[] findTempsLessThan(double tempValue){
+    public double[] findTempsLessThan(double tempValue) {
         double[] tempLessList = new double[numOfElements];
         int size = 0;
         for (double el: temperatureList){
-            if (el < tempValue){
+            if (el < tempValue) {
                 tempLessList[size] = el;
                 size += 1;
             }
@@ -87,11 +92,11 @@ public class TemperatureSeriesAnalysis {
         return tempLessList;
     }
 
-    public double[] findTempsGreaterThan(double tempValue){
+    public double[] findTempsGreaterThan(double tempValue) {
         double[] tempGreaterList = new double[numOfElements];
         int size = 0;
-        for (double el: temperatureList){
-            if (el > tempValue){
+        for (double el: temperatureList) {
+            if (el > tempValue) {
                 tempGreaterList[size] = el;
                 size += 1;
             }
@@ -99,20 +104,23 @@ public class TemperatureSeriesAnalysis {
         return tempGreaterList;
     }
 
-    TempSummaryStatistics summaryStatistics(){
-        if (this.numOfElements == 0)
+    TempSummaryStatistics summaryStatistics() {
+        if (this.numOfElements == 0) {
             return (TempSummaryStatistics) IllegalArgumentException;
+        }
         TempSummaryStatistics statistic = new TempSummaryStatistics(average(), deviation(), min(), max());
 
         return statistic;
 
     }
     
-    public int addTemps(double[] temps){
-        for (double el: temps)
-            if (el < -273)
+    public int addTemps(double[] temps) {
+        for (double el: temps) {
+            if (el < LOWESTTEMP) {
                 throw new InputMismatchException();
-        while (temperatureList.length - numOfElements < temps.length){
+            }
+        }
+        while (temperatureList.length - numOfElements < temps.length) {
             double[] newTemperatureList = new double[2 * numOfElements];
             System.arraycopy(temperatureList, 0, newTemperatureList, 0, numOfElements);
             temperatureList = newTemperatureList;
